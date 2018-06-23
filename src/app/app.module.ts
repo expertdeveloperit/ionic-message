00d854c208e8d72bcda5d874011d240da2c44bc7
Expert <expertdeveloperit>
2018-06-23 14:41:27 +0530

    dummy app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,28 +3,48 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { HttpClientModule } from '@angular/common/http';
+
 
 import { MyApp } from './app.component';
+
+// Pages
 import { HomePage } from '../pages/home/home';
+import { SearchPage } from '../pages/search/search';
+import { TermsNPolicyPage } from '../pages/terms-n-policy/terms-n-policy';
+import { AuthPage } from "../pages/auth/auth";
+import { ActivationCodePage } from '../pages/activation-code/activation-code';
+import { TextEditorPage } from '../pages/text-editor/text-editor';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    SearchPage,
+    TermsNPolicyPage,
+    AuthPage,
+    ActivationCodePage,
+    TextEditorPage
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    SearchPage,
+    TermsNPolicyPage,
+    AuthPage,
+    ActivationCodePage,
+    TextEditorPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
   ]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { SearchPage } from '../search/search';
+import { TermsNPolicyPage } from '../terms-n-policy/terms-n-policy';
+import { TextEditorPage } from '../text-editor/text-editor';
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
@@ -11,7 +13,18 @@ export class HomePage {
 
   }
 
-  navigate(){
-    this.navCtrl.push(SearchPage);
+  navigate(pageName: String){
+    switch(pageName){
+      case 'search':
+        this.navCtrl.push(SearchPage);
+        break;
+      case 'terms':
+        this.navCtrl.push(TermsNPolicyPage);
+        break;
+    }
+  }
+
+  sendMessage(){
+    this.navCtrl.push(TextEditorPage);
   }
 }
